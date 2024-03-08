182b7959216aa7660b5b6861a9d9571759ded007
DESKTOP-O3ICS2S\Valera <valerarslvskijj@yandex.ru>
2024-03-08 15:47:16 +0300

    Checkpoint

---
+++ Portfolio/Source/Portfolio/Game/PortfolioSaveGame.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/SaveGame.h"
+#include "PortfolioSaveGame.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PORTFOLIO_API UPortfolioSaveGame : public USaveGame
+{
+	GENERATED_BODY()
+	
+};
---
+++ Portfolio/Source/Portfolio/HUD/HUDInterfaceSettings.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "HUDInterfaceSettings.h"
+
+// Add default functionality here for any IHUDInterfaceSettings functions that are not pure virtual.
